66121fa329866eac626aabeb34758bd5cac76293
Alexandros Georgiadis <kokoras19888@gmail.com>
2025-01-26 09:35:20 +0100

    adding questions

--- src/data/questions.jsx
+++ src/data/questions.jsx
@@ -13,7 +13,12 @@ const questions = {
   firstAid: [
     { question: "How do we treat someone who feels fainting", answer: "Elevate the legs" },
     { question: "How do you treat a burn injury?", answer: "Cool the burn under running water and cover it with a clean cloth." },
-    { question: "What should you do if someone is choking?", answer: "Perform the Heimlich maneuver or encourage them to cough." }
+    { question: "What should you do if someone is choking?", answer: "Perform the Heimlich maneuver or encourage them to cough." },
+    {question:"What are the PDI's of water glycol?", answer: "1. Stowage, 2. Quandity, 3. Wire seal on handle is intact, 4. Cartige is visible in handle"},
+    {question:"What are the PDI's of halon?", answer: "1. Stowage, 2. Quandity, 3. Preassure gauge must be in the serviceable band, 4. Pin is secure in the handle"},
+    {question:"What are the PDI's of fire gloves?", answer: "1. Stowage, 2. Quandity, 3. No scroch marks, 4. One left ine right"},
+    {question:"What are the PDI's of torch?", answer: "1. Stowage, 2. Quandity, 3. No scroch marks, 4. Red light flashes at not more than 10 seconds"},
+    {question:"What are the PDI's of portsble oxygen mask?", answer: "1. Stowage, 2. Quandity, 3. Strap is present, 4. Gauge reads 3/4 to full, 5. Mask attached to Hi flow for emergency use bottles, 6. Mask attached to low flow for therapeutic use bottles, 7. Spare mask is present"},
   ]
 };
 
